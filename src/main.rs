653bd78c31760fc24a91e58e5ccc1bdcb3c69fa2
Maarten van Schaik <maarten@informaarten.nl>
2022-12-01 23:18:25 +0000

    Completed day 1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::collections::BinaryHeap;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let mut heap = BinaryHeap::new();
+    let mut cur_elf = 0;
+
+    for line in io::stdin().lock().lines() {
+        let line = line.unwrap();
+        if line == "" {
+            heap.push(cur_elf);
+            cur_elf = 0;
+            continue;
+        }
+        cur_elf += line.parse::<i32>().unwrap();
+    }
+    heap.push(cur_elf);
+
+    let mut top3 = 0;
+    top3 += heap.pop().unwrap();
+    println!("Step 1: max is {}", top3);
+    top3 += heap.pop().unwrap();
+    top3 += heap.pop().unwrap();
+    println!("Step 2: top3 is {}", top3);
 }
